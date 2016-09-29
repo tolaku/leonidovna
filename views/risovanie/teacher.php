50f2086b3ecd69c7dd9e7a999178cb1cd6991f1c
Anatol Kulesh <tola_k@mail.ru>
2016-09-29 22:28:25 +0300

    uch

--- views/risovanie/teacher.php
+++ views/risovanie/teacher.php
@@ -4,7 +4,7 @@
                     <?php foreach($sections as $section): ?>
                     	<div class="col-3">
                         	<h1><?=$section['name']?></h1>
-                        	<img src="<?=$section['img']?>"/>
+                        	<img src="<?=TEMPLATE?><?=$section['img']?>"/>
                             <!-- выводим текст -->
                             <?=$section['text_min']?>
                         </div>
@@ -13,23 +13,12 @@
                     <div class="clr"></div>
                     <div class="inner">
                     	<div class="col-2">
-                        	<h2><a href="#">Vijay Raj</a></h2>
+                        	<h2><a href="#">Светлана Леонидовна</a></h2>
                             <img src="img/team-01.jpg"/>
-                            <p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitt accusam.</p>
-                            <p class="read"><a href="#" title="Follow him on Twitter">@iamdesigner</a></p>
+                            <p>Люблю вышивать по вечерам. Пишу стихи, езжу на рыбалку )).</p>
+                            <p class="read"><a href="#" title="Follow him on Twitter">@leonedovna</a></p>
                         </div>
-                        <div class="col-2">
-                        	<h2><a href="#">Chris Neil</a></h2>
-                            <img src="img/team-02.jpg"/>
-                            <p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitt accusam.</p>
-                            <p class="read"><a href="#" title="Follow him on Twitter">@chrisneil</a></p>
-                        </div>
-                        <div class="col-2">
-                        	<h2><a href="#">Micky Smith</a></h2>
-                            <img src="img/team-03.jpg"/>
-                            <p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitt accusam.</p>
-                            <p class="read"><a href="#" title="Follow him on Twitter">@mickys</a></p>
-                        </div>
+             
                     </div>
             </div>
             <div class="clr"></div>
