52bf0791d0faec77eef67fa2d35f05b169559410
Anatol Kulesh <tola_k@mail.ru>
2016-10-10 19:30:55 +0300

    page_admin

--- admin/templates/leftbar.php
+++ admin/templates/leftbar.php
@@ -8,26 +8,17 @@
                                     <img src="<?=TEMPLATE_ADMIN?>img/26115.jpg" class="img-circle" alt="User Image" />
                                 </div>
                                 <div class="pull-left info">
-                                    <p>Hello, Jane</p>
+                                    <p>Привет, Светлана Леонидовна</p>
 
                                     <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
                                 </div>
                             </div>
-                            <!-- search form -->
-                            <form action="#" method="get" class="sidebar-form">
-                                <div class="input-group">
-                                    <input type="text" name="q" class="form-control" placeholder="Search..."/>
-                                    <span class="input-group-btn">
-                                        <button type='submit' name='seach' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i></button>
-                                    </span>
-                                </div>
-                            </form>
-                            <!-- /.search form -->
+
                             <!-- sidebar menu: : style can be found in sidebar.less -->
                             <ul class="sidebar-menu">
                                 <li class="active">
                                     <a href="index.html">
-                                        <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+                                        <i class="fa fa-dashboard"></i> <span>Страницы</span>
                                     </a>
                                 </li>
                                 <li>
