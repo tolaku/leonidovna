2a9dcf5fd49b2878cb0d3a5fab13fc7c05e17e8d
Anatol <tola_k@mail.ru>
2016-10-18 23:34:11 +0300

    razdel

--- admin/templates/leftbar.php
+++ admin/templates/leftbar.php
@@ -17,7 +17,7 @@
                             <!-- sidebar menu: : style can be found in sidebar.less -->
                             <ul class="sidebar-menu">
                                 <li class="active">
-                                    <a href="index.html">
+                                    <a href="?view=pages">
                                         <i class="fa fa-dashboard"></i> <span>Страницы</span>
                                     </a>
                                 </li>
