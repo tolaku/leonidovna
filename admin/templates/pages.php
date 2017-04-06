bcf039247032df9d279721056d9bacf550f19773
Anatol Kulesh <tola_k@mail.ru>
2017-04-06 08:24:20 +0300

    main_beauty

--- admin/templates/pages.php
+++ admin/templates/pages.php
@@ -26,26 +26,18 @@
                               <tbody>
                                 <tr>
                                     <td>1</td>
-                                    <td>Главная</td>
+                                    <td><a href="?view=sections">Главная</a></td>
                                     <td>
-                                      <div class="pull-right hidden-phone">
-                                        
-                                        <a href="?view=sections"><button class="btn btn-default btn-xs"><i class="fa fa-pencil"></i></button></a>
-                                      </div>
+
                                     </td>
                                 </tr>
                               <?php $i=1; ?>
                               <?php foreach($pages as $item): $i++; ?>
                                 <tr>
                                   <td><?=$i?></td>
-                                  <td><?=$item['name']?></td>
+                                    <td><a href="?view=<?=$item['url_page']?>"><?=$item['name']?></a></td>
                                   <td>
-                                    <div class="pull-right hidden-phone">
-                                        
-                                        <a href="?view=<?=$item['url_page']?>">
-                                          <button class="btn btn-default btn-xs"><i class="fa fa-pencil"></i></button>
-                                        </a>
-                                    </div>
+
                                   </td>
                                 </tr>
                               <?php endforeach; ?>
@@ -58,47 +50,7 @@
           </div><!--end col-6 -->
           
                     </div>
-                    <div class="row">
-                        
-                        <div class="col-md-7">
-                          <section class="panel tasks-widget">
-                              <header class="panel-heading">
-                                  Todo list
-                            </header>
-                            <div class="panel-body">
-
-                              <div class="task-content">
-
-                                  <ul class="task-list">
-                                      <li>
-                                          <div class="task-checkbox">
-                                              <!-- <input type="checkbox" class="list-child" value=""  /> -->
-                                              <input type="checkbox" class="flat-grey list-child"/>
-                                              <!-- <input type="checkbox" class="square-grey"/> -->
-                                          </div>
-                                          <div class="task-title">
-                                              <span class="task-title-sp">Director is Modern Dashboard</span>
-                                              <span class="label label-success">2 Days</span>
-                                              <div class="pull-right hidden-phone">
-                                                  <button class="btn btn-default btn-xs"><i class="fa fa-check"></i></button>
-                                                  <button class="btn btn-default btn-xs"><i class="fa fa-pencil"></i></button>
-                                                  <button class="btn btn-default btn-xs"><i class="fa fa-times"></i></button>
-                                              </div>
-                                          </div>
-                                      </li>
-                                      
-
-                                  </ul>
-                              </div>
-
-                              <div class=" add-task-row">
-                                  <a class="btn btn-success btn-sm pull-left" href="#">Add New Tasks</a>
-                                  <a class="btn btn-default btn-sm pull-right" href="#">See All Tasks</a>
-                              </div>
-                          </div>
-                      </section>
-                  </div>
-              </div>
+                    
               <!-- row end -->
                 
 
